7e02ab7111f0813656de3cd5c4a33137cc5a5a8e
AbelGAlem <abelzeraftechnologies@gmail.com>
2025-08-14 11:47:28 +0300

    feat(web): add Tailwind CSS configuration and improv mockup section with minimum height

--- apps/web/components/mockup.tsx
+++ apps/web/components/mockup.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function MokcupSection() {
     return(
-        <section className="bg-[url(/mockupBG.png)] bg-cover flex justify-center items-center mt-16 lg:mt-24">
+        <section className="bg-[url(/mockupBG.png)] bg-cover flex justify-center items-center mt-16 lg:mt-24 min-h-[10rem] md:min-h-[24rem]">
             <div className="relative w-[55%] md:w-1/2 translate-x-12 md:translate-x-20 lg:translate-x-28">
                 <Image
                     src="/mockup.png"
@@ -12,6 +12,7 @@ export default function MokcupSection() {
                     height={0}
                     sizes="50vw"
                     className="w-full h-auto object-contain"
+                    priority
                 />
             </div>
         </section>
---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,21 @@
+export default {
+  safelist: [
+    // text colors
+    "text-red-700",
+    "text-yellow-700",
+    "text-green-700",
+    // borders
+    "border-red-500",
+    "border-yellow-500",
+    "border-green-500",
+    // backgrounds
+    "bg-red-50",
+    "bg-yellow-50",
+    "bg-green-50",
+    "bg-red-100",
+    "bg-yellow-100",
+    "bg-green-100",
+  ],
+} as const
+
+
